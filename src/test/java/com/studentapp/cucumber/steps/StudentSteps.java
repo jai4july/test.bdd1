3dbc3370f43adb1037e24dd3b79267f83cea2896
jai4july <jai4july@gmail.com>
2018-06-24 17:23:56 -0400

    jun24
    june24

--- src/test/java/com/studentapp/cucumber/steps/StudentSteps.java
+++ src/test/java/com/studentapp/cucumber/steps/StudentSteps.java
@@ -3,7 +3,10 @@ package com.studentapp.cucumber.steps;
 import cucumber.api.java.en.When;
 import net.serenitybdd.rest.SerenityRest;
 
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,10 +16,12 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 import cucumber.api.java.en.Then;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
 public class StudentSteps {
 	
 	
 	@When("^Check if the status code is 200 and correct$")
+	@Title("TEST 1")
 	public void verify_status_code_200_for_listendpoints() {
 		SerenityRest.rest()
 		.given()
@@ -38,4 +43,22 @@ public class StudentSteps {
 		
 	}
 	
-}
+	@When("^Make sure the file you want to access is available$")
+	public void verify_file_exist() throws IOException {
+		File f =new File("E://file.txt");
+		FileReader fr= new FileReader(f);
+		fr.close();
+	}
+	
+	@When("^Do not test this its a manual test case$")
+	public void test_manual_testcase()
+	{
+		
+	}
+	
+	@When("^Testing negative scenario dividing by 0$")
+	public void test_negative_scenario()
+	{
+		System.out.println("This is an error" +(5/0));
+	}
+	}
--- src/test/resources/feature/student.feature
+++ src/test/resources/feature/student.feature
@@ -1,12 +1,22 @@
-Feature: Testing different requests on the student application
+Feature: CASE ROUTING MULESOFT API
 
 @SMOKE
-Scenario: Check if the student application can be accessed by users
+Scenario: CASE ROUTING POSITIVE SCENARIO WITH TERM LIFE
 When Check if the status code is 200 and correct
 
 
 @SMOKE
-Scenario: Application issues should be resolved
+Scenario: CASE ROUTING POSITIVE SCENARIO WITH WHOLE LIFE
 When Make sure the application has the necessary requirements checked
 
- 
+@SMOKE
+Scenario: CASE ROUTING NEGATIVE SCENARIO WITHOUT PRODUCT CODE VALUE
+When Make sure the file you want to access is available
+
+@Manual	
+Scenario: CASE ROUTING NEGATIVE SCENARIO WITHOUT CASETYPE VALUE
+When Do not test this its a manual test case
+
+@NEGATIVE
+Scenario: CASE ROUTING NEGATIVE SCENARIO WITHOUT GoCODE PARAM
+When Testing negative scenario dividing by 0
